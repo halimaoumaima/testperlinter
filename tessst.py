d7206b5fe7db1aabea66a0c6ee0fd9f6d8f9a108
halimaoumaima <117830774+halimaoumaima@users.noreply.github.com>
2022-11-23 14:41:13 +0100

    Update tessst.py

--- tessst.py
+++ tessst.py
@@ -1,10 +1,10 @@
 user_input = input("Input text to translate to pig latin: ")
 print("User Text: ", user_input)
-updated_user_input = user_input.split(' ')
+updated_user_input = user_input.split(" ")
 for j in updated_user_input:
- if len(j) >= 3: #only translate words with more than 3 characters
-  j = j + "%say" % (j[0]) 
-  j = j[1:]
-  print(j)
- else:
-  pass
+    if len(j) >= 3:  # only translate words with more than 3 characters
+        j = j + "%say" % (j[0])
+        j = j[1:]
+        print(j)
+    else:
+        pass
